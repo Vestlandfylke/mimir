01e33aa3b1c3e09535d4f554132457e32369e6b3
Trond Farestveit Kvittingen <Trond.Farestveit.Kvittingen@vlfk.no>
2024-09-27 12:59:21 +0200

    fixed dependancies versions and deployment

--- scripts/deploy/deploy-azure.ps1
+++ scripts/deploy/deploy-azure.ps1
@@ -48,7 +48,7 @@ param(
 
     [string]
     # Region to which to make the deployment
-    $Region = "southcentralus",
+    $Region = "Sweden Central",
 
     [string]
     # SKU for the Azure App Service plan
